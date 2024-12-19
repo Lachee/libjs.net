c689eb3beb102407808242c22218d77c26baf829
Lachee <lachlan.h@hotmail.com>
2024-12-20 09:43:10 +1100

    updated build.bat to output a bin file.

--- build.bat
+++ build.bat
@@ -1,8 +1,16 @@
 @echo off
 
-:: Build the project
+mkdir -p bin
+
+:: Build the C++ project
+echo "Building Unmanaged Libraries..."
 cmake --preset windows . 
 cd build/release || exit /b 1
 ninja || exit /b 1
+cp -r *.dll ../../bin
 cd ../../
 
+echo "Building Managed Libraries..."
+cd library || exit /b 1
+dotnet build -o ../bin || exit /b 1
+cd ../
