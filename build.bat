530d16a3952c41c8119a81a9119b1d2968c7d452
Lachee <lachlan.h@hotmail.com>
2024-12-20 09:50:33 +1100

    updated build scripts

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-mkdir -p bin
+mkdir bin
 
 :: Build the C++ project
 echo "Building Unmanaged Libraries..."
@@ -10,6 +10,7 @@ ninja || exit /b 1
 cp -r *.dll ../../bin
 cd ../../
 
+:: Build the C# project
 echo "Building Managed Libraries..."
 cd library || exit /b 1
 dotnet build -o ../bin || exit /b 1
--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+
+mkdir bin
+
 # Build the project
 cmake --preset default .
 cd Build/release
 ninja
+cp -r *.so ../../bin
 cd ../../
 
-
+# Build the C# project
+echo "Building Managed Libraries..."
+cd library || exit /b 1
+dotnet build -o ../bin || exit /b 1
+cd ../
