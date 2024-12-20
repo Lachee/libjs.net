458d8a9f40bb871d6397e6960207ae576b90cdd9
Lachee <lachlan.h@hotmail.com>
2024-12-20 11:12:36 +1100

    replaced ninja

--- build.bat
+++ build.bat
@@ -4,8 +4,8 @@ mkdir bin
 
 :: Build the C++ project
 echo "Building Unmanaged Libraries..."
-cmake --preset windows . 
-cmake --build Build/release
+cmake --preset windows . || exit /b 1
+cmake --build Build/release || exit /b 1
 cp -r Build/release/*.dll bin
 
 :: Build the C# project
