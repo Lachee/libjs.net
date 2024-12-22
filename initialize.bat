e9092e90528c591d747fc74809935968e29289cd
Lachee <lachlan.h@hotmail.com>
2024-12-23 09:40:52 +1100

    tweaked initialize.bat

--- initialize.bat
+++ initialize.bat
@@ -1,5 +1,15 @@
+@echo off
+setlocal enabledelayedexpansion
+
+echo == Initializing Submodules
 git submodule update --init
-patch.bat apply
-mkdir Build
+
+echo == Applying Patches
+call patch.bat apply || echo "Patch failed, continuing anyway..."
+
+echo == Building Vcpkg
+if not exist "Build" mkdir "Build"
 cp -r ladybird/Toolchain Toolchain
 python ./Toolchain/BuildVcpkg.py
+
+echo Initialization complete.
